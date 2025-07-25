9198d883d2808392a9cf9d8137c52eabab304383
Carlos Eduardo Ibarra Manzano <carlosibarra999@outlook.com>
2025-07-24 21:46:02 -0500

    Implement AsyncStorage for name management and update dependencies

--- App.tsx
+++ App.tsx
@@ -1,28 +1,96 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { Button, StyleSheet, Text, TextInput, TouchableHighlight, View } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { useEffect, useState } from 'react'
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+export default function App() {
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [name, setName] = useState('')
+  const [nameStorage, setNameStorage] = useState('')
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const name = await AsyncStorage.getItem('name') ?? ''
+        setNameStorage(name)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    getData()
+  }, [])
+
+  const saveData = async () => {
+    try {
+      await AsyncStorage.setItem('name', name)
+      setNameStorage(name)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const deleteData = async () => {
+    try {
+      await AsyncStorage.removeItem('name')
+      setNameStorage('')
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
     <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+      {nameStorage && (
+        <Text>Hello: {nameStorage}</Text>
+      )}
+
+      <TextInput
+        placeholder='Type your name'
+        style={styles.input}
+        onChangeText={text => setName(text)}
+      />
+
+      <Button
+        title='Save name'
+        color='#333'
+        onPress={() => saveData()}
+      />
+
+      {nameStorage && (
+        <TouchableHighlight
+          onPress={() => deleteData()}
+          style={styles.deleteBtn}
+        >
+          <Text style={styles.deleteBtnText}>Delete name &times;</Text>
+        </TouchableHighlight>
+      )}
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
-});
-
-export default App;
+  input: {
+    borderColor: '#666',
+    borderBottomWidth: 1,
+    width: 300,
+    height: 40
+  },
+  deleteBtn: {
+    backgroundColor: 'red',
+    marginTop: 10,
+    padding: 10
+  },
+  deleteBtnText: {
+    color: '#FFF',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    textTransform: 'uppercase',
+    width: 300
+  }
+})
